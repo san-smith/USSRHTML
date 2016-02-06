e184fe3d91e17922a4b3c9bc9a46588670444014
san-smith <san-smith@mail.ru>
2016-02-06 17:16:32 +0600

    Тег "\тело()" теперь работает правильно. Добавлен пример helloworld

--- USSRHTML.py
+++ USSRHTML.py
@@ -1,15 +1,74 @@
 '''
-
-@author: Smith
+USSRHTML - язык гипертекстовой разметки (ЯГТР), котрый мог быть создан в СССР
 '''
+# импортируем sys для возможности получения файла с исходным текстом в качестве аргумента
+import sys
 
-import USSRHTMLCore
+# Проверяем наличие аргумента
+if len(sys.argv) > 1:
+    # Считываем файл по указанному пути
+    try:
+        f = open(sys.argv[1])
+        lines = f.readlines()
+        print(lines)
+        text = ''.join(lines)
+        f.close()
+    except IOError:
+        print('Невозможно открыть файл! Проверьте правильность пути.')
+        exit()
+        
+else:
+    print('Ошибка! Путь к файлу не указан!')
+    exit()
+    
+# Разбиваем файл на голову и тело
+if text.find('\тело(') == -1:
+    print('Ошибка!')
+    exit()
+else:
+    head = text[:text.index('\тело(')]
+    body = text[text.index('\тело('):]
 
-f = open('./sample/index.txt','w')
-f.write('\ягтр(\голова()\тело(Привет, Мир!))')
-f.close()
 
-f = open('./sample/index.txt')
-line = f.readline()
-print(line)
-f.close()
+# Список возможных команд
+commands = ["\тело(", "\ж(", "\к("]
+
+# Стек состояний
+statement = []
+
+# Создаем файл с тем же имененем и расширением .html
+fout = open(sys.argv[1][:-3] + 'html', 'w')
+# Записываем в файл 
+fout.write('<html>\n')
+
+i = len("\тело(")
+
+STATE = '<body>'            # Текущее состояние
+statement.append(STATE)     # Добавляем текущее состояние в стек состояний
+fout.write(STATE + '\n')    # И записываем его в выходной файл
+
+# Начинаем разбор текста
+s = ''
+while i < len(body):
+    # Если текущее состояние <body> 
+    if STATE == '<body>':
+        if body[i] == '\\':
+            # Здесь будет разбор новых команд
+            pass
+        
+        elif (body[i] == ')'):
+            # Если текущее состояние <body> и встретили ")", то 
+            fout.write(s + '\n')        # записываем предыдущий текст
+            fout.write('</body>\n')     # и конец тела документа
+            break                       # выходим, т.к. дальше ничего быть не должно
+        else:
+            s += body[i]    # Если просто буква, то добавляем её к тексту           
+    
+    i += 1
+
+
+fout.write('</html>\n')
+fout.close()
+
+
+
